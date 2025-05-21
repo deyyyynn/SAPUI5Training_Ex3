84af7092a58e4ba40e2f7078ce9f74215deda6d1
Dane Mark ABAD <dane-mark.abad-ext@vinci-construction.com>
2025-05-21 05:00:24 +0000

    Initial Commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sapips.training.ex2.controller.View1", {
+        onInit() {
+        }
+    });
+});

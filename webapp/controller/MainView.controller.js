a4d4f32b2c3e20573bcb0f725a6470014197f5f8
Dane Mark Abad <dane-mark.abad-ext@vinci-construction.com>
2025-06-02 05:27:01 +0000

    Routes-Navigation-DMA-EX6-001

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -12,11 +12,36 @@ function (Controller, MessageToast) {
         onInit() {
         },
 
-        onAddItem: function (){
+        onAddItem: function () {
             var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
             var sMsg = oTextBundle.getText("addButtonMsg");
             this.fnDisplayMsg(sMsg);
+
+            // Comment this code for now
+            // var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            // var sMsg = oTextBundle.getText("addButtonMsg");
+            // this.fnDisplayMsg(sMsg);
+
+            // Instantiate the fragment
+
+            // create dialog lazily
+            if (!this.oDialog) {
+                // By using loadFragment, we are adding the fragment as a dependent to the View
+                // By doing so, we can use the functions inside the view's controller
+                this.oDialog = this.loadFragment({
+                    name: "com.training.exer1abad.fragment.ProductDialog"
+                });
+            }
+            this.oDialog.then(function (oDialog) {
+                oDialog.open();
+            });
         },
+
+        onCloseDialog: function () {
+            this.getView().byId("idProductDialog").close();
+        },
+
+
 
         fnDisplayMsg: function (sMsg){
             MessageToast.show(sMsg);
@@ -56,18 +81,30 @@ function (Controller, MessageToast) {
         },
 
         onPressCheckout: function (){
-            var oInputFNameValue = this.getView().byId("idInptFName").getValue();
-            var oInputLNameValue = this.getView().byId("idInptLName").getValue();
-            
-            var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-            var sMsg = oTextBundle.getText("errReqFieldMsg");
-            // Check if first name is blank
-            if (oInputFNameValue === "" ){
-                sap.m.MessageToast.show(sMsg); 
-            } else if(oInputFNameValue === "" && oInputLNameValue === ""){
-                sap.m.MessageToast.show(sMsg); 
+            var oInputFName = this.getView().byId("idInptFName");
+            var oInputLName = this.getView().byId("idInptLName");
+            var oInputFNameValue = oInputFName.getValue();
+            var oInputLNameValue = oInputLName.getValue();
+            var oRouter = this.getOwnerComponent().getRouter();
+
+            // Check if first name and last name is blank
+            if (oInputFNameValue === "" || oInputLNameValue === "") {
+
+                // set value state to Error
+                oInputFName.setValueState("Error");
+                oInputLName.setValueState("Error");
+            } else {
+                oInputFName.setValueState("None");
+                oInputLName.setValueState("None");
+
+                //Navigate to review page passing first
+                oRouter.navTo("RouteReviewPage", {
+                    firstName: oInputFNameValue
+                });
+
             }
         },
+
 
 
     })  ;
